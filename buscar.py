167d72364fc9c1da939238b3fdbfcc98faf00dc0
andressr8 <93979868+andressr8@users.noreply.github.com>
2022-03-08 13:05:25 +0100

    Update buscar.py

--- buscar.py
+++ buscar.py
@@ -1,3 +1,4 @@
+#Este es el código de la búsqueda de Andrés Serrano Rodríguez, sirve para realizar búsquedas en la agenda a partir de nombres de usuarios.
 def buscar_ASerrano(nombre, agenda):
         for nombre in agenda.items():
             if nombre.startswith(búsqueda):
