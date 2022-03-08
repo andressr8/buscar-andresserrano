33d58738e9de344bd2bbea8a994b55d3cecfc4d2
andressr8 <93979868+andressr8@users.noreply.github.com>
2022-03-08 12:44:59 +0100

    Add files via upload

---
+++ buscar.py
@@ -0,0 +1,9 @@
+def buscar_ASerrano(nombre, agenda):
+        for nombre in agenda.items():
+            if nombre.startswith(búsqueda):
+                print("Estos son los contactos " % (nombre,agenda[nombre]))   
+
+
+
+
+
